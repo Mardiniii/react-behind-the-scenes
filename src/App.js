97888f0dfea4d3092c23890e4ef4cadfbea78ab1
mardiniii <sezama03@hotmail.com>
2021-06-09 08:27:44 -0400

    Quick example to show React basic work to render elements

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import './App.css';
+import Button from "./components/UI/Button/Button";
+
+import "./App.css";
 
 function App() {
+  const [showParagraph, setShowParagraph] = useState(false);
+
+  console.log("App Running!"); // It runs everytime the component is re-evaluated
+
+  const toggleParagraphHandler = () => {
+    setShowParagraph((prevShowParagraph) => {
+      return !prevShowParagraph;
+    });
+  };
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      {showParagraph && <p>This is new!</p>}
+      <Button onClick={toggleParagraphHandler}>Show Paragraph</Button>
     </div>
   );
 }
